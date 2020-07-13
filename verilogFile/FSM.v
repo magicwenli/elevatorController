d62f3311c18c49d211569eadda08eff654c9ff69
magicwenli <yxra3603@outlook.com>
2020-07-14 01:41:03 +0800

    修复fsm模块的bug

--- verilogFile/FSM.v
+++ verilogFile/FSM.v
@@ -5,46 +5,112 @@ module FSM(
     output reg F0
 	);
 	
+reg currentF1;
+reg currentF0;
+
+initial begin
+currentF1=1;
+currentF0=0;
+end
+
 always @(*)
 begin
     case(current)
         4'b0001: if(|signal[7:1]) begin F1 <=1; F0<=0; end   // floor 1
                     else begin F1 <=0; F0<=0; end
-        4'b0010: if(|signal[7:2]) begin F1 <=1; F0<=0; end   // floor 2
-                    else begin 
-                    if(|signal[0]) begin F1 <=0; F0<=1; end
-                    else begin F1 <=0; F0<=0; end 
+        4'b0010: if({currentF1,currentF0}==2'b10) begin
+                        if(|signal[7:2]) begin F1 <=1; F0<=0; end   // floor 2
+                        else begin 
+                        if(|signal[0]) begin F1 <=0; F0<=1; end
+                        else begin F1 <=0; F0<=0; end 
+                        end
                     end
-        4'b0011: if(|signal[7:3]) begin F1 <=1; F0<=0; end
-                    else begin 
-                    if(|signal[1:0]) begin F1 <=0; F0<=1; end
-                    else begin F1 <=0; F0<=0; end 
+                 else begin
+                        if(|signal[0]) begin F1 <=0; F0<=1; end
+                        else begin 
+                        if(|signal[7:2]) begin F1 <=1; F0<=0; end
+                        else begin F1 <=0; F0<=0; end 
+                        end
                     end
-        4'b0100: if(|signal[7:4]) begin F1 <=1; F0<=0; end
-                    else begin 
-                    if(|signal[2:0]) begin F1 <=0; F0<=1; end
-                    else begin F1 <=0; F0<=0; end 
+        
+        4'b0011: if({currentF1,currentF0}==2'b10) begin
+                        if(|signal[7:3]) begin F1 <=1; F0<=0; end   // floor 3
+                        else begin 
+                        if(|signal[1:0]) begin F1 <=0; F0<=1; end
+                        else begin F1 <=0; F0<=0; end 
+                        end
                     end
-        4'b0101: if(|signal[7:5]) begin F1 <=1; F0<=0; end
-                    else begin 
-                    if(|signal[3:0]) begin F1 <=0; F0<=1; end
-                    else begin F1 <=0; F0<=0; end 
+                 else begin
+                        if(|signal[1:0]) begin F1 <=0; F0<=1; end
+                        else begin 
+                        if(|signal[7:3]) begin F1 <=1; F0<=0; end
+                        else begin F1 <=0; F0<=0; end 
+                        end
                     end
-        4'b0110: if(|signal[7:6]) begin F1 <=1; F0<=0; end
-                    else begin 
-                    if(|signal[4:0]) begin F1 <=0; F0<=1; end
-                    else begin F1 <=0; F0<=0; end 
+        4'b0100: if({currentF1,currentF0}==2'b10) begin
+                        if(|signal[7:4]) begin F1 <=1; F0<=0; end   // floor 4
+                        else begin 
+                        if(|signal[2:0]) begin F1 <=0; F0<=1; end
+                        else begin F1 <=0; F0<=0; end 
+                        end
                     end
-        4'b0111: if(|signal[7]) begin F1 <=1; F0<=0; end
-                    else begin 
-                    if(|signal[5:0]) begin F1 <=0; F0<=1; end
-                    else begin F1 <=0; F0<=0; end 
+                 else begin
+                        if(|signal[2:0]) begin F1 <=0; F0<=1; end
+                        else begin 
+                        if(|signal[7:4]) begin F1 <=1; F0<=0; end
+                        else begin F1 <=0; F0<=0; end 
+                        end
                     end
-        4'b1000: if(|signal[6:0]) begin F1 <=0; F0<=1; end   // floor 8
+        4'b0101: if({currentF1,currentF0}==2'b10) begin
+                        if(|signal[7:5]) begin F1 <=1; F0<=0; end   // floor 5
+                        else begin 
+                        if(|signal[3:0]) begin F1 <=0; F0<=1; end
+                        else begin F1 <=0; F0<=0; end 
+                        end
+                    end
+                 else begin
+                        if(|signal[3:0]) begin F1 <=0; F0<=1; end
+                        else begin 
+                        if(|signal[7:5]) begin F1 <=1; F0<=0; end
+                        else begin F1 <=0; F0<=0; end 
+                        end
+                    end
+        4'b0110: if({currentF1,currentF0}==2'b10) begin
+                        if(|signal[7:6]) begin F1 <=1; F0<=0; end   // floor 6
+                        else begin 
+                        if(|signal[4:0]) begin F1 <=0; F0<=1; end
+                        else begin F1 <=0; F0<=0; end 
+                        end
+                    end
+                 else begin
+                        if(|signal[4:0]) begin F1 <=0; F0<=1; end
+                        else begin 
+                        if(|signal[7:6]) begin F1 <=1; F0<=0; end
+                        else begin F1 <=0; F0<=0; end 
+                        end
+                    end
+        4'b0111: if({currentF1,currentF0}==2'b10) begin
+                        if(|signal[7]) begin F1 <=1; F0<=0; end      // floor 7
+                        else begin 
+                        if(|signal[5:0]) begin F1 <=0; F0<=1; end
+                        else begin F1 <=0; F0<=0; end 
+                        end
+                    end
+                 else begin
+                        if(|signal[5:0]) begin F1 <=0; F0<=1; end
+                        else begin 
+                        if(|signal[7]) begin F1 <=1; F0<=0; end
+                        else begin F1 <=0; F0<=0; end 
+                        end
+                    end
+        4'b1000: if(|signal[6:0]) begin F1 <=0; F0<=1; end           // floor 8
                     else begin F1 <=0; F0<=0; end
-        default: if(|signal[7:1]) begin F1 <=1; F0<=0; end   // floor 1
+        default: if(|signal[7:1]) begin F1 <=1; F0<=0; end           // floor 1
                     else begin F1 <=0; F0<=0; end
     endcase
+
+    currentF0 <= F0;
+    currentF1 <= F1;
 
 end
 
--- verilogFile/clock.v
+++ verilogFile/clock.v
@@ -7,42 +7,42 @@ module clock(
     output reg[6:0]sm_seg
     );
     
-    reg [3:0]timesec0;
-    
-    initial begin
-    sm_bit=1;
-    sm_seg=1;
-    timesec0=0;
+reg [3:0]timesec0;
+
+initial begin
+sm_bit=1;
+sm_seg=1;
+timesec0=0;
+end
+
+always @(posedge clk) begin
+        if(pause) begin
+            if(timesec0==9) timesec0=0;
+        else timesec0=timesec0+1;  
     end
-    
-    always @(posedge clk) begin
-    if(pause) begin
-    if(timesec0==9)
-    timesec0=0;
+        if(~pause) timesec0=0;
+end
+
+always@(posedge show or negedge rst)
+begin
+    if(!rst)
+        begin
+        sm_seg = 7'b0000000;
+        end
     else
-    timesec0=timesec0+1;  
-    end
-    if(~pause) begin
-    timesec0=0;
-    end
-    end
-    
-    always@(posedge show)
-    begin
-    case(timesec0)
-            0:sm_seg= 7'b1111110;                     //显示0
-            1:sm_seg= 7'b0110000;                     //显示1
-            2:sm_seg= 7'b1101101;                     //显示2
-            3:sm_seg= 7'b1111001;                     //显示3
-            4:sm_seg= 7'b0110011;                     //显示4
-            5:sm_seg= 7'b1011011;                     //显示5
-            6:sm_seg= 7'b1011111;                     //显示6
-            7:sm_seg= 7'b1110000;                     //显示7
-            8:sm_seg= 7'b1111111;                     //显示8
-            9:sm_seg= 7'b1111011;                     //显示9
-            default:
-            sm_seg= 7'b0000000;                        //不显示
-    endcase
-    end
-    
+        case(timesec0)
+                0:sm_seg= 7'b1111110;                     //显示0
+                1:sm_seg= 7'b0110000;                     //显示1
+                2:sm_seg= 7'b1101101;                     //显示2
+                3:sm_seg= 7'b1111001;                     //显示3
+                4:sm_seg= 7'b0110011;                     //显示4
+                5:sm_seg= 7'b1011011;                     //显示5
+                6:sm_seg= 7'b1011111;                     //显示6
+                7:sm_seg= 7'b1110000;                     //显示7
+                8:sm_seg= 7'b1111111;                     //显示8
+                9:sm_seg= 7'b1111011;                     //显示9
+                default:
+                sm_seg= 7'b0000000;                        //不显示
+        endcase
+end
 endmodule
